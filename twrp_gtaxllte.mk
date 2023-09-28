aac3738000c1c75d60754a0ac02c9e2899dd33c1
Kareem Ebrahem <safen894@gmail.com>
2023-09-28 15:52:12 +0300

    gtaxllte: Update and rename omni_gtaxllte.mk to twrp_gtaxllte.mk ; And change product name to twrp_gtaxllte.

--- twrp_gtaxllte.mk
+++ twrp_gtaxllte.mk
@@ -5,7 +5,7 @@ PRODUCT_RELEASE_NAME := gtaxllte
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_PACKAGES += \
 	charger_res_images \
@@ -14,7 +14,7 @@ PRODUCT_PACKAGES += \
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gtaxllte
 PRODUCT_MODEL := SM-T585
-PRODUCT_NAME := omni_gtaxllte
+PRODUCT_NAME := twrp_gtaxllte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 
